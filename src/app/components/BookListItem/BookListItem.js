c6373bf5f927f2b9150d32f85f632ac1be0f91e7
Timm Schärli <timm.schaerli@gmail.com>
2022-06-27 19:34:00 +0200

    Iteration 2

--- src/app/components/BookListItem/BookListItem.js
+++ src/app/components/BookListItem/BookListItem.js
@@ -1,16 +1,89 @@
 import {getBookThumbnail} from "../../service/googleapi";
 import './BookListItem.css';
+import {useState} from "react";
 
 function BookListItem(props) {
-    return (
-        <div className="col">
-            <div className="card shadow-sm">
-                <img src={getBookThumbnail(props.book.isbn)} alt={props.book.title}/>
+    let [isEdit, setEdit] = useState(false),
+     [title, setTitle] = useState(props.book.title),
+     [author, setAuthor] = useState(props.book.author),
+     [pages, setPages] = useState(props.book.pages),
+     [total_amount, setTotalAmount] = useState(props.book.total_amount),
+     [isbn, setISBN] = useState(props.book.isbn)
+
+    const editButtonPressed = () => {
+        setEdit(true)
+    }
+
+
+    const onKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            const b = props.book
+            if (title+author+pages+total_amount+isbn !== b.title+b.author+b.pages+b.total_amount+b.isbn) {
+                props.onBookDidChange({
+                    id: props.book.id,
+                    title: title !== "" ? title : props.book.title,
+                    author: author !== "" ? author : props.book.author,
+                    pages: pages !== "" ? pages : props.book.pages,
+                    total_amount: total_amount !== "" ? total_amount : props.book.total_amount,
+                    isbn: isbn !== "" ? isbn : props.book.isbn
+                })
+            }
+            setEdit(false);
+        }
+    }
+
+    const renderChangedButtons = (props) => {
+        if (props.book.edited) {
+            return <div className="editedButtons">
+                <button onClick={onRefreshClicked} className="btn btn-primary">Reload Book</button>
+                <button onClick={onSubmitChanges} className="btn btn-primary">Submit Changes</button></div>
+        }
+    }
+
+    const onRefreshClicked = () => {
+        props.onBookRefresh(props.book.id)
+    }
+
+    const onSubmitChanges = () => {
+        props.onBookSubmit(props.book.id, {title, author, pages, total_amount, isbn})
+    }
+
+    const renderCardContent = (props) => {
+        if (isEdit) {
+            return (<div className="card-body">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">title</span>
+                        <input type="text" className="form-control" placeholder="title" aria-label="title" value={title}
+                               aria-describedby="basic-addon1" onKeyDown={onKeyDown} onChange={(e) => {setTitle(e.target.value)}}/>
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">author</span>
+                        <input type="text" className="form-control" placeholder="author" aria-label="author" value={author}
+                               aria-describedby="basic-addon1" onKeyDown={onKeyDown} onChange={(e) => {setAuthor(e.target.value)}}/>
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">total amount</span>
+                        <input type="text" className="form-control" placeholder="total_amount" aria-label="total_amount" value={total_amount}
+                               aria-describedby="basic-addon1" onKeyDown={onKeyDown} onChange={(e) => {setTotalAmount(e.target.value)}}/>
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">ISBN</span>
+                        <input type="text" className="form-control" placeholder="isbn" aria-label="isbn" value={isbn}
+                               aria-describedby="basic-addon1" onKeyDown={onKeyDown}
+                               onChange={(e) => {setISBN(e.target.value)}}/>
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">pages</span>
+                        <input type="text" className="form-control" placeholder="pages" aria-label="pages" value={pages}
+                               aria-describedby="basic-addon1" onKeyDown={onKeyDown} onChange={(e) => {setPages(e.target.value)}}/>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
                 <div className="card-body">
                     <div className="title">
-                    <h3>
-                        {props.book.title}
-                    </h3>
+                        <h3>{props.book.title}</h3>
                     </div>
                     <p className="card-text">
                         Amount: {props.book.total_amount} <br/>
@@ -19,12 +92,24 @@ function BookListItem(props) {
                     </p>
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="btn-group">
-                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button type="button" className="btn btn-sm btn-outline-secondary"
+                                    onClick={editButtonPressed}>Edit
+                            </button>
                             <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
                         </div>
                         <small className="text-muted">{props.book.pages} Pages</small>
                     </div>
-                </div>
+                </div>)
+        }
+    }
+
+
+    return (
+        <div className="col">
+            <div className="card shadow-sm">
+                {renderChangedButtons(props)}
+                <img src={getBookThumbnail(props.book.isbn)} alt={props.book.title}/>
+                {renderCardContent(props)}
             </div>
         </div>
     );
--- src/app/container/BooksListPage/BooksListPage.js
+++ src/app/container/BooksListPage/BooksListPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {getBooks} from '../../service/mockapi'
+import {getBook, getBooks, putBook} from '../../service/mockapi'
 import BookListItem from "../../components/BookListItem/BookListItem";
 import BooksList from "../../components/BooksList/BooksList";
 import Header from "../../components/Header/Header";
+import './BooksListPage.css';
 
 class BooksListPage extends React.Component {
 
@@ -15,16 +16,65 @@ class BooksListPage extends React.Component {
 
     async componentDidMount() {
         getBooks().then(books => {
-            this.setState({books: books})
+            this.setState({
+                books: books.map(b => {
+                    return {
+                        edited: false,
+                        ...b
+                    }
+                })
+            })
+        })
+    }
+
+    bookDidChange = (bookChange) => {
+        this.setState({
+            books: this.state.books.map(book => {
+                if (book.id === bookChange.id) {
+                    return {
+                        edited: true,
+                        ...bookChange
+                    }
+                }
+                return book
+            })
+        })
+    }
+
+    submitBookChanges = (id, book) => {
+        putBook(id, book).then(updatedBook => {
+            this.setState({
+                books: this.state.books.map(book => {
+                    if (book.id === updatedBook.id) {
+                        return updatedBook
+                    }
+                    return book
+                })
+            })
+        })
+    }
+
+    refreshBook = (id) => {
+        getBook(id).then(refreshedBook => {
+            this.setState({
+                books: this.state.books.map(book => {
+                    if (book.id === refreshedBook.id) {
+                        return refreshedBook
+                    }
+                    return book
+                })
+            })
         })
     }
 
     render() {
         return (<div>
-            <Header/>
+                <Header/>
                 <BooksList>
                     {this.state.books.map(book => (
-                        <BookListItem key={book.id} book={book}/>
+                        <BookListItem key={book.id} book={book} onBookDidChange={this.bookDidChange}
+                                      onBookSubmit={this.submitBookChanges}
+                                      onBookRefresh={this.refreshBook}/>
                     ))}
                 </BooksList>
             </div>
